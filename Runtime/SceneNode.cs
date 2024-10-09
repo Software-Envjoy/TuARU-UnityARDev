b2a8054a380e37fd7d1dcbd6d3fbe35415c5b845
Jesus Moreno Puertas <jesus@morenopuertas.es>
2024-10-09 05:27:11 +0200

    doc: Add missing xml tags

--- Runtime/SceneNode.cs
+++ Runtime/SceneNode.cs
@@ -2,15 +2,31 @@
 
 namespace Envjoy.GLTF
 {
+    /// <summary>
+    /// Defines the <see cref="SceneNode" />
+    /// </summary>
     public class SceneNode : MonoBehaviour
     {
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets the ImageTarget
+        /// </summary>
         [field: SerializeField]
         public Texture ImageTarget { get; set; }
 
+        /// <summary>
+        /// Gets or sets the Width
+        /// </summary>
         [field: SerializeField]
         public float Width { get; set; } = 1.0f;
 
+        /// <summary>
+        /// Gets or sets the Orientation
+        /// </summary>
         [field: SerializeField]
         public Orientation Orientation { get; set; }
+
+        #endregion
     }
 }
